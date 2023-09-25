0c5d5367476acbb1123817837774e49ec34285eb
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-09-25 17:33:44 +0500

    angular bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   // template:"<div></div>",
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'tail-ng';
+  message = "Silience";
+  editmsg = "I am read only";
+   canEdit = false;
+  sayHello() {
+    alert(this.message);
+  };
+onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = "I'm read only!";
+    }
+  }
 }
