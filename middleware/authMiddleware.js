7240efe971e0aa5fd7408a1ff9cdb0bda19dd547
Abdul Haseeb <abdulhaseebcoder@gmail.com>
2024-10-18 16:19:16 +0000

    formatted

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,16 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+  const token = req.header("Authorization");
+  if (!token)
+    return res.status(401).json({ message: "No token, authorization denied" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Token is not valid' });
+    res.status(401).json({ message: "Token is not valid" });
   }
 };
 
